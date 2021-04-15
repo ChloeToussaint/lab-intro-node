2a6d48d92f386303f534cd28cf7ec734f40d539c
chloe toussaint <toussaintchloe@hotmail.fr>
2021-04-15 21:28:57 +0200

    iteration5

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+this.items = [];
+this.length = this.items.length;
 
-  add(item) {}
+  }
 
-  get(pos) {}
+  add(item) {
+this.items.push (item);
+this.items.sort((a, b) => a - b);
+this.length = this.items.length;
+  }
 
-  max() {}
+  get(pos) {
+    if (pos > this.length) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items [pos];
+  }
 
-  min() {}
+  max() {
+if (this.length <=0) {
+  throw new Error('EmptySortedList');
+}
+return Math.max (...this.items)
+  }
+
+  min() {
+    if (this.length <=0) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.min (...this.items)
+  }
 
   sum() {}
 
